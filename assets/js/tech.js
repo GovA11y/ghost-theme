9e2b988bcc48d95e2b8e6484e8e57bfdc00b4853
Bentley Hensel <bentleyhensel@gmail.com>
2023-09-08 19:13:53 -0400

    Improvments

--- assets/js/tech.js
+++ assets/js/tech.js
@@ -1,22 +1,28 @@
 document.getElementById('techCheckForm').addEventListener('submit', function(e) {
   e.preventDefault();
-  const url = document.getElementById('url').value;
+  let url = document.getElementById('url').value;
   const resultsDiv = document.getElementById('results');
-  const loader = document.getElementById('loader');
+  const loader = document.querySelector('.loader');
 
-  resultsDiv.innerHTML = '';
-  loader.style.display = 'block';  // Show loader
+  // Ensure the input is a valid URL
+  if (!url.startsWith('http://') && !url.startsWith('https://')) {
+    url = 'http://' + url;
+  }
+
+  loader.style.display = 'block'; // Show loader
+  resultsDiv.style.display = 'none'; // Hide results
 
   fetch(`https://tech.gova11y.io/extract?url=${url}`)
     .then(response => response.json())
     .then(data => {
-      loader.style.display = 'none';  // Hide loader
+      loader.style.display = 'none'; // Hide loader
+      resultsDiv.style.display = 'block'; // Show results
       displayResponseData(data);
     })
     .catch(error => {
       console.error('Error:', error);
-      loader.style.display = 'none';  // Hide loader
-      resultsDiv.textContent = 'An error occurred while fetching the data. Please try again.';
+      loader.style.display = 'none'; // Hide loader
+      resultsDiv.textContent = 'An error occurred while fetching the data. Please try again.'; // Display error message
     });
 });
 
@@ -49,40 +55,67 @@ function displayResponseData(data) {
 }
 
 function groupTechnologiesByCategory(technologies) {
-  return technologies.reduce((categories, tech) => {
-    tech.categories.forEach(category => {
-      if (!categories[category.name]) {
-        categories[category.name] = [];
-      }
-      categories[category.name].push(tech);
-    });
-    return categories;
+  return technologies.reduce((acc, tech) => {
+    const { category } = tech;
+    if (!acc[category]) acc[category] = [];
+    acc[category].push(tech);
+    return acc;
   }, {});
 }
 
 function createTechCard(tech) {
-  const card = document.createElement('div');
-  card.className = 'tech-card';
-  card.innerHTML = `
-    <div class="tech-header">
-      <img src="https://placehold.co/50x50?${tech.icon}.svg" alt="${tech.name} icon" />
-      <h3><a href="${tech.website}" target="_blank">${tech.name}</a> (${tech.version || 'No version available'})</h3>
-    </div>
-    <p>${tech.description || 'No description available'}</p>
-    <div class="chart-container">
-      <div class="confidence-chart" style="width: ${tech.confidence}%;">
-        ${tech.confidence}%
-      </div>
-    </div>
-  `;
-  return card;
+  const techCard = document.createElement('div');
+  techCard.className = 'tech-card';
+
+  const header = document.createElement('div');
+  header.className = 'tech-header';
+  const img = document.createElement('img');
+  img.src = `https://placehold.co/50x50?${tech.icon}.svg`;
+  header.appendChild(img);
+  const title = document.createElement('h3');
+  title.textContent = tech.name;
+  header.appendChild(title);
+  techCard.appendChild(header);
+
+  const version = document.createElement('p');
+  version.textContent = tech.version ? `Version: ${tech.version}` : '';
+  techCard.appendChild(version);
+
+  const confidenceContainer = document.createElement('div');
+  confidenceContainer.className = 'chart-container';
+  const confidenceChart = document.createElement('div');
+  confidenceChart.className = 'confidence-chart';
+  confidenceChart.style.width = `${tech.confidence}%`;
+  confidenceChart.textContent = `${tech.confidence}%`;
+  confidenceContainer.appendChild(confidenceChart);
+  techCard.appendChild(confidenceContainer);
+
+  const confidenceLabel = document.createElement('p');
+  confidenceLabel.className = 'confidence-label';
+  confidenceLabel.textContent = 'Confidence';
+  techCard.appendChild(confidenceLabel);
+
+  const moreInfoBtn = document.createElement('a');
+  moreInfoBtn.href = `${tech.website}`;
+  moreInfoBtn.className = 'more-info-btn';
+  moreInfoBtn.textContent = 'More Info';
+  techCard.appendChild(moreInfoBtn);
+
+  return techCard;
 }
 
 function displayURLResponseData(urls) {
   const urlsDiv = document.getElementById('urls');
-  urlsDiv.innerHTML = `
-    ${Object.entries(urls).map(([url, data]) => `
-      <p>${url}: ${data.status} ${data.redirected ? `(Redirected to: ${data.url})` : ''}</p>
-    `).join('')}
-  `;
+  urlsDiv.innerHTML = '';
+  urlsDiv.style.display = 'block';
+
+  const urlsHeader = document.createElement('h2');
+  urlsHeader.textContent = 'URL Response Data';
+  urlsDiv.appendChild(urlsHeader);
+
+  Object.entries(urls).forEach(([url, status]) => {
+    const urlDiv = document.createElement('div');
+    urlDiv.textContent = `${url}: ${status}`;
+    urlsDiv.appendChild(urlDiv);
+  });
 }
